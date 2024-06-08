6524fb4e230f5ad444243cd4aed62281272d984d
Minji Yun <minjiyun404@gmail.com>
2024-06-08 16:50:45 -0700

    main gui updated

---
+++ flask_ml_gui/static/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "Stock Predictor";
+    let index = 0;
+    const speed = 100; // Adjust the speed of the typing effect
+
+    function typeWriter() {
+        if (index < text.length) {
+            document.getElementById("typewriter").innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeWriter, speed);
+        } else {
+            // Remove the blinking cursor after typing is done
+            document.getElementById("typewriter").style.borderRight = "none";
+        }
+    }
+
+    typeWriter();
+});
